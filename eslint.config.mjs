e37b6289b12eec7ae2ba3de5191600f458b8684a
Changming Sun <chasun@microsoft.com>
2025-03-26 23:39:37 -0700

    update

---
+++ eslint.config.mjs
@@ -0,0 +1,35 @@
+import { defineConfig, globalIgnores } from "eslint/config";
+import globals from "globals";
+import path from "node:path";
+import { fileURLToPath } from "node:url";
+import js from "@eslint/js";
+import { FlatCompat } from "@eslint/eslintrc";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const compat = new FlatCompat({
+    baseDirectory: __dirname,
+    recommendedConfig: js.configs.recommended,
+    allConfig: js.configs.all
+});
+
+export default defineConfig([
+    globalIgnores(["**/node_modules/", "**/build/", "**/dist/", "**/coverage/", "**/*.log"]),
+    {
+        extends: compat.extends("standard"),
+
+        languageOptions: {
+            globals: {
+                ...globals.node,
+            },
+
+            ecmaVersion: "latest",
+            sourceType: "commonjs",
+        },
+
+        rules: {
+            semi: ["error", "always"],
+            "comma-dangle": ["error", "always-multiline"],
+        },
+    },
+]);
